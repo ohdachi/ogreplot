776b02d46f42908ffc142fbb9063aae3917f18b5
ohdachi.satoshi <ohdachi.satoshi@ba2397a4-557e-11de-8ef9-cf35a3c3eb08>
2009-12-28 02:50:19 +0000

    add contour function
    git-svn-id: https://ogreplot.googlecode.com/svn/trunk@27 ba2397a4-557e-11de-8ef9-cf35a3c3eb08

---
+++ contourtest.rb
@@ -0,0 +1,18 @@
+require 'graph'
+
+data = (0 .. 99).collect{|i| Array.new(100)}
+(0 .. 99).each{|i|
+  (0 .. 99).each{|j|
+    data[j][i] = Math::exp( - ((i.to_f/10.0 - 4.5)**2 + (j.to_f/10.0 - 4.5)**2)/2.0 ) * Math::sin( Math::atan2(j.to_f/10.0-4.5, i.to_f/10.0-4.5) * 3)
+  }
+}
+
+xarr = (0 .. 99).collect{|i| i.to_f/10.0}
+yarr = (0 .. 99).collect{|i| i.to_f/10.0}
+c = Graph.new(data, 0, 1, :gtype => Ogre::Contour, :xarr => xarr, :yarr => yarr, :xrange => [2, 7], :yrange => [-1, 11], :levels => [-0.05, -0.01, 0.01, 0.05, 0.1, 0.2, 0.3] )
+c.xaxis.range = [2, 7]
+c.levels = [-0.05, -0.01, 0.01, 0.05, 0.1, 0.2, 0.3]
+PSCanvas.new('test.ps') do |ps|
+  c.plot(ps)
+end
+  
